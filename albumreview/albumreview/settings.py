563c5808a296b8d374ab1df9607ec743ef23006f
Kayliegh.Hill <kayliegh.hill@gmail.com>
2020-09-02 10:56:37 -0400

    app init

--- albumreview/albumreview/settings.py
+++ albumreview/albumreview/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'albumreview.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'albumreview',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+
     }
 }
 
