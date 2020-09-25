8029b46a426e5bfb52560a9b4a4a9f2499482dc1
Kayliegh.Hill <kayliegh.hill@gmail.com>
2020-09-25 16:33:54 -0400

    import django_heroku to settings.py

--- albumreview/albumreview/settings.py
+++ albumreview/albumreview/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
