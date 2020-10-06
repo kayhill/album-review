d16479ac482c1eb3c51baeb6857381df934d6955
Kayliegh.Hill <kayliegh.hill@gmail.com>
2020-10-06 15:15:26 -0400

    javascript for avatars

--- aotw/static/aotw/app.js
+++ aotw/static/aotw/app.js
@@ -1,9 +1,18 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   // side navigation menu  
-    var sidenav = document.querySelector('.sidenav');
-    M.Sidenav.init(sidenav);
+  var sidenav = document.querySelector('.sidenav');
+  M.Sidenav.init(sidenav);
 
-    var carousel = document.querySelectorAll('.carousel');
-    M.Carousel.init(carousel);
+  var carousel = document.querySelectorAll('.carousel');
+  M.Carousel.init(carousel);
 
-  });
+  geturl();
+});
+
+function geturl() {
+  const pics = document.querySelectorAll('.user_avatar');
+  for (let pic in pics) {
+    url = pic.src;
+    url.replace('/resized/80/', '/');
+  }
+}
